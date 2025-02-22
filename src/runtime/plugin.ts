168ace56fa01026ab69acd5f175f04691f673283
litingyes <luz.liting@gmail.com>
2025-02-22 11:50:57 +0800

    perf(nuxt): get pinia logic

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,13 +1,12 @@
+import type { Pinia } from 'pinia'
 import { defineNuxtPlugin } from '#app'
 import { createPiniaUnstorage } from '../index'
 
 export default defineNuxtPlugin({
   name: 'pinia-plugin-unstorage',
-  setup({ vueApp }) {
-    const options = useRuntimeConfig().public.piniaUnstorage ?? {}
+  setup({ $pinia }) {
+    const options = (useRuntimeConfig().public.piniaUnstorage ?? {});
 
-    const pinia = vueApp.config.globalProperties.$pinia
-
-    pinia.use(createPiniaUnstorage(options))
+    ($pinia as Pinia).use(createPiniaUnstorage(options))
   },
 })
