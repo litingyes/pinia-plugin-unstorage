95e23f11987c102ce607871abdebe17f7a2b438b
litingyes <luz.liting@gmail.com>
2025-02-22 12:33:55 +0800

    feat: add pick/omit options for store

--- examples/vue/src/stores/count.ts
+++ examples/vue/src/stores/count.ts
@@ -5,8 +5,18 @@ export const useCountStore = defineStore('count', () => {
   const count = ref(1)
   const add = () => ++count.value
 
+  const str = ref('s')
+  const append = () => `${str.value}s`
+
   return {
     count,
     add,
+
+    str,
+    append,
   }
+}, {
+  unstorage: {
+    omit: ['str'],
+  },
 })
--- src/plugin.ts
+++ src/plugin.ts
@@ -2,7 +2,18 @@ import type { PiniaPlugin } from 'pinia'
 import type { Driver } from 'unstorage'
 import { destr } from 'destr'
 import localStorageDriver from 'unstorage/drivers/localstorage'
+import { omit, pick } from 'usexx'
 import { isClient, toRaws } from './utils'
+
+declare module 'pinia' {
+  // eslint-disable-next-line unused-imports/no-unused-vars
+  export interface DefineStoreOptionsBase<S, Store> {
+    unstorage?: {
+      pick?: string[]
+      omit?: string[]
+    }
+  }
+}
 
 export interface UnstorageOptions {
   namespace?: string
@@ -11,6 +22,18 @@ export interface UnstorageOptions {
 
 export const DEFAULT_UNSTORAGE_OPTIONS: UnstorageOptions = {
   namespace: 'pinia',
+}
+
+function extractData(data: Record<string, unknown>, pickKeys?: string[], omitKeys?: string[]) {
+  if (pickKeys?.length) {
+    return pick(data, pickKeys)
+  }
+
+  if (omitKeys?.length) {
+    return omit(data, omitKeys)
+  }
+
+  return data
 }
 
 export function createPiniaUnstorage(_globalOptions?: UnstorageOptions) {
@@ -23,13 +46,17 @@ export function createPiniaUnstorage(_globalOptions?: UnstorageOptions) {
     })
   }
 
-  return (({ store }) => {
+  return (({ store, options }) => {
+    const { pick: pickKeys, omit: omitKeys } = options.unstorage ?? {}
+
     Promise.resolve(storage?.getItem(store.$id)).then((val) => {
-      store.$patch(destr(val))
+      const data = destr<Record<string, unknown>>(val)
+
+      store.$patch(extractData(data, pickKeys, omitKeys))
     })
 
     store.$subscribe(async (_, state) => {
-      await storage?.setItem!(store.$id, JSON.stringify(toRaws(state)), {})
+      await storage?.setItem!(store.$id, JSON.stringify(extractData(toRaws(state), pickKeys, omitKeys)), {})
     })
   }) as PiniaPlugin
 }
